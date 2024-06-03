09633b0d8fca814339af886cd4387528cee2aa22
ooTATooo <kd1298535@st.kobedenshi.ac.jp>
2024-06-03 14:20:01 +0900

    レイ判定完成、アニメーション変数宣言、初期化

--- 02_quarterview/Src/Application/Object/Player/Player.cpp
+++ 02_quarterview/Src/Application/Object/Player/Player.cpp
@@ -1,5 +1,7 @@
 ﻿#include "Player.h"
 
+#include "../../Scene/SceneManager.h"
+
 void Player::Update()
 {
 	m_dir = Math::Vector3::Zero;
@@ -12,6 +14,8 @@ void Player::Update()
 	m_dir.Normalize();
 
 	m_pos += m_dir * m_spped;
+
+	if (GetAsyncKeyState(VK_SPACE) & 0x8000) { m_gravity = -0.1f; }
 
 	// 重力をキャラに反映
 	m_pos.y -= m_gravity;
@@ -31,16 +35,54 @@ void Player::Update()
 	rayinfo.m_pos = m_pos;
 
 	// 少し高いところから飛ばす
-	rayinfo.m_pos.y += 0.1f;
+	//rayinfo.m_pos.y += 0.1f;
+
+	// 段差の許容範囲を設定
+	static const float enableStepHigh = 0.2f;
+	rayinfo.m_pos.y += enableStepHigh;
 
 	// レイの方向を設定
 	rayinfo.m_dir = { 0.0f,-1.0f,0.0f };
 
 	// レイの長さを確定
-	rayinfo.m_range = m_gravity;
+	rayinfo.m_range = m_gravity + enableStepHigh;
 
 	// 当たり判定をしたいタイプを設定
 	rayinfo.m_type = KdCollider::TypeGround;
+
+	// デバッグ用の情報としてライン描画を追加
+	m_pDebugWire->AddDebugLine(rayinfo.m_pos, rayinfo.m_dir, rayinfo.m_range);
+
+	// レイに当たったオブジェクト情報を格納するリスト
+	std::list<KdCollider::CollisionResult> retRayList;
+
+	// 作成したレイ情報でオブジェクトと当たり判定をする
+	for (auto& obj : SceneManager::Instance().GetObjList())
+	{
+		obj->Intersects(rayinfo, &retRayList);
+	}
+
+	// レイに当たったリストから一番近いオブジェクトを検出
+	bool hit = false;
+	float maxOverLap = 0;
+	Math::Vector3 groundPos = {};
+
+	for (auto& ret : retRayList)
+	{
+		// レイが当たった場合の貫した長さが一番長いものを探す
+		if (maxOverLap < ret.m_overlapDistance)
+		{
+			maxOverLap = ret.m_overlapDistance;
+			groundPos = ret.m_hitPos;
+			hit = true;
+		}
+	}
+
+	if (hit)
+	{
+		m_pos = groundPos;
+		m_gravity = 0;
+	}
 
 	//======================================
 	// 当たり判定		レイ判定	ここまで
@@ -59,11 +101,24 @@ void Player::Init()
 	m_poly = std::make_shared<KdSquarePolygon>();
 	m_poly->SetMaterial("Asset/Textures/Player.png");
 	m_poly->SetSplit(4, 8);
+
+	// 初期アニメーション情報
+	m_animeInfo.start = 4;
+	m_animeInfo.end = 7;
+	m_animeInfo.count = 0;
+	m_animeInfo.speed = 0.2f;
+
+	// 向いている方向
+	m_dirType = DirType::Down;
+
 	m_poly->SetPivot(KdSquarePolygon::PivotType::Center_Bottom);
 	m_pos = m_mWorld.Translation();
 	m_spped = 0.1f;
 	m_gravity = 0.0f;
 	m_mWorld = Math::Matrix::Identity;
+
+	// デバッグワイヤー生成
+	m_pDebugWire = std::make_unique<KdDebugWireFrame>();
 }
 
 void Player::GenerateDepthMapFromLight()
--- 02_quarterview/Src/Application/Object/Player/Player.h
+++ 02_quarterview/Src/Application/Object/Player/Player.h
@@ -4,6 +4,23 @@ class Player :public KdGameObject
 {
 public:
 
+	enum DirType
+	{
+		Up = 1 << 0,
+		Down = 1 << 1,
+		Left = 1 << 2,
+		Right = 1 << 3,
+	};
+
+	// アニメーション情報
+	struct AnimationInfo
+	{
+		int start;
+		int end;
+		float count;
+		float speed;
+	};
+
 	Player() {}
 	~Player()override {}
 
@@ -16,6 +33,12 @@ public:
 private:
 
 	std::shared_ptr<KdSquarePolygon> m_poly;
+
+	// アニメーション情報
+	AnimationInfo m_animeInfo;
+
+	// キャラが向いている方向種類　ビット管理
+	UINT m_dirType;
 
 	Math::Vector3 m_pos;
 
